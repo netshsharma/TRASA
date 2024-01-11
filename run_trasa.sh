9d6e9a7c92c7dfa9770a199f2e3c8fe92f09ef7a
Netsh Sharma <80478405+netshsharma@users.noreply.github.com>
2024-01-11 10:18:52 +0545

    Update run_trasa.sh

--- run_trasa.sh
+++ run_trasa.sh
@@ -11,13 +11,13 @@ sudo docker run -d --rm --name guacd -p 127.0.0.1:4822:4822 -v /tmp/trasa/access
 
 # Run TRASA server
 sudo docker run -d \
---link db:db \
---link guacd:guacd \
---link redis:redis \
--p 443:443 \
--p 80:80 \
--p 8022:8022 \
--e TRASA.LISTENADDR=0.0.0.0 \
--e TRASA.AUTOCERT="false" \
--v /tmp/trasa/accessproxy/guac:/tmp/trasa/accessproxy/guac \
-seknox/trasa:v1.1.4
+  --link db:db \
+  --link guacd:guacd \
+  --link redis:redis \
+  -p 8443:443 \
+  -p 8080:80 \
+  -p 8022:8022 \
+  -e TRASA.LISTENADDR=0.0.0.0 \
+  -e TRASA.AUTOCERT="false" \
+  -v /tmp/trasa/accessproxy/guac:/tmp/trasa/accessproxy/guac \
+  seknox/trasa:v1.1.4
